18ea4e2ff67f8cb614d1e2f7f48b06bcd143ea94
/Seyed <seyedkia27@gmail.com>
2017-01-30 20:49:12 +0330

    Update Core.lua

--- plugins/Core.lua
+++ plugins/Core.lua
@@ -357,10 +357,10 @@ if data.first_name_ then
       username = 'ندارد'
     end
 
-    local text1 = "📜 اطلاعات کاربر :\nشناسه : [*"..data.id_.."*]\nنام کاربری : "..username.."\nنام کاربر :"
+    local text1 = "📜 اطلاعات کاربر :\nشناسه : [*"..data.id_.."*]\nنام کاربری : "..username.."\nنام کاربر : "..data.first_name_
             
     --tdcli.sendMessage(arg.chat_id, 0, 1, "📜 اطلاعات کاربر :\nشناسه : [*"..data.id_.."*]\nنام کاربری : "..username.."\nنام کاربر : _"..data.first_name_.."_\n", 1, "md")
-    tdcli_function ({ID="SendMessage", chat_id_=arg.chat_id, reply_to_message_id_="", disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=text1, disable_web_page_preview_=1, clear_draft_=0, entities_={[0]={ID="MessageEntityMentionName", offset_=0, length_=string.len(data.first_name_), user_id_=data.id_}}}}, dl_cb, nil)            
+    tdcli_function ({ID="SendMessage", chat_id_=arg.chat_id, reply_to_message_id_="", disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=text1, disable_web_page_preview_=1, clear_draft_=0, entities_={[0]={ID="MessageEntityMentionName", offset_=0, length_= string.sub(text1, -string.len(data.first_name_)), user_id_=data.id_}}}}, dl_cb, nil)            
   else
     return tdcli.sendMessage(arg.chat_id, "", 0, "🚫 _مشخصات کاربر پیدا نشد_ !", 0, "md")
   end
